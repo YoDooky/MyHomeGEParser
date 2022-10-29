a3bf1a6805a41a68a8602b95860d844311f09d7d
YoDooky <anurovan@gmail.com>
2022-10-30 02:01:21 +0400

    Parser completed. Need to make telegram bot for sending data

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 from collectData import DataCollect
 import config
 import os.path
+from console_progressbar import ProgressBar
 
 
 def update_json_file(page_data, filename='data.json'):
@@ -13,18 +14,42 @@ def update_json_file(page_data, filename='data.json'):
             json.dump(page_data, file)
     with open(filename, 'r', encoding='utf-8') as file:
         file_data = json.load(file)
+        current_data = {}
         for key, value in page_data.items():
             if key in file_data:
                 break
-            new_data = {**{key: value}, **file_data}  # add new data to start of the dict
-    if not new_data:
-        return
+            current_data[key] = value
+        new_data = {**current_data, **file_data}  # add new data to start of the dict
+    if not current_data:
+        return 1
     with open(filename, 'w', encoding='utf-8') as file:
         json.dump(new_data, file)
 
 
 def run_full_scan():
     """Runs full scan for selected city"""
+
+
+def get_cities_json():
+    """Save all cities to JSON"""
+    url = 'https://www.myhome.ge/en/search/getCities'
+    headers = config.header
+    req = requests.post(url, headers=headers)
+    src = req.text
+    with open('cities_raw.json', 'w', encoding='utf-8') as file:
+        file.write(src)
+    with open('cities_raw.json', encoding='utf-8') as file:  # test
+        python_src = json.load(file)
+    cities_list = [each['name']['en'] for each in python_src['subLocs']]
+    cities_json = json.dumps(cities_list)
+    with open('cities.json', 'w', encoding='utf-8') as file:
+        file.write(cities_json)
+
+
+def get_cities():
+    with open('cities.json', encoding='utf-8') as file:
+        cities_list = json.load(file)
+    return cities_list
 
 
 def get_pages_amount(city):
@@ -41,8 +66,8 @@ def get_page_src(city, page_number=1):
     header = config.header
     req = requests.get(url, headers=header)
     src = req.text
-    with open('data.html', 'w', encoding='utf-8') as file:
-        file.write(src)
+    # with open('data.html', 'w', encoding='utf-8') as file:
+    #     file.write(src)
     return src
 
 
@@ -58,17 +83,18 @@ def get_page_data(src):
 
 
 def main():
-    city = 'Batumi'  # test
+    city = get_cities()[46]  # test Kobuleti
     filename = f'./data/{city}.json'
-    # pages_amount = int(get_pages_amount(city))
-    # for page in range(1, pages_amount + 1):
-    #     src = get_page_src(city, page)
-    with open('data.html', encoding='utf-8') as file:  # test
-        src = file.read()
-    page_data = get_page_data(src)
-
-    update_json_file(page_data, filename)
+    pages_amount = int(get_pages_amount(city))
+    pb = ProgressBar(total=pages_amount, prefix='Here', suffix='Now', decimals=0, length=50, fill='X', zfill='-')
+    for page in range(pages_amount, 0, -1):
+        src = get_page_src(city, page)
+        page_data = get_page_data(src)
+        if update_json_file(page_data, filename):
+            break
+        pb.print_progress_bar(pages_amount - page)
 
 
 if __name__ == '__main__':
+    get_cities()
     main()
--- test.py
+++ test.py
@@ -1,3 +1,7 @@
-d = {'primary': 'value1', 'secondary': 'value2'}
-if 'pr' in d:
-    print('Yo')
+import time
+from console_progressbar import ProgressBar
+
+pb = ProgressBar(total=10, prefix='Here', suffix='Now', decimals=0, length=50, fill='X', zfill='-')
+for i in range(10):
+    pb.print_progress_bar(i)
+    time.sleep(1)
